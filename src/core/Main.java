5cf34d138ae59fc0f2d28b5f0b0b02aae088bfa8
Mateus Resende <mateus.rsnd@gmail.com>
2016-12-04 16:06:08 -0200

    Remove maven do projeto

---
+++ src/core/Main.java
@@ -0,0 +1,16 @@
+package core;
+
+
+
+import screens.HomeScreen;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	HomeScreen homeScreen = new HomeScreen();
+        
+        homeScreen.setVisible(true);
+    }
+
+
+}
